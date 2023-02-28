213d6335482f8bdad9263a644dc0a61c9dbea47b
Rashed Arman <rashedarman21@gmail.com>
2023-02-28 19:45:58 +0600

    refactor: Round avg weight in query

--- queries.sql
+++ queries.sql
@@ -66,7 +66,7 @@ SELECT * FROM animals;
 --    What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
-SELECT AVG(weight_kg) FROM animals;
+SELECT ROUND(AVG(weight_kg)) FROM animals;
 SELECT neutered, SUM(escape_attempts) as escape_attempts FROM animals GROUP BY neutered ORDER BY escape_attempts DESC LIMIT 1;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
