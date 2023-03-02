32605d7052bd67df56ead6295ce99726e676ec1c
Rashed Arman <rashedarman21@gmail.com>
2023-03-02 22:36:16 +0600

    refactor: combine alter table statements

--- schema.sql
+++ schema.sql
@@ -26,20 +26,10 @@ CREATE TABLE IF NOT EXISTS species (
 );
 
 -- Modify animals table:
+    -- Make sure that id is set as autoincremented PRIMARY KEY
+    -- Remove column species
+    -- Add column species_id which is a foreign key referencing species table
+    -- Add column owner_id which is a foreign key referencing the owners table
 
--- Make sure that id is set as autoincremented PRIMARY KEY
-    -- this now makes animals.id a primary key, which was not the case before.
-    -- you can validate thi by running command `\d+ animals` in postgresql.
 ALTER TABLE animals ADD PRIMARY KEY (id);
-
--- Remove column species
--- ALTER TABLE animals DROP COLUMN species;
-
--- Add column species_id which is a foreign key referencing species table
--- ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
-
--- Add column owner_id which is a foreign key referencing the owners table
--- ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
-
--- Note: I like to combine the above three ALTER statements since they are all modifying the same table.
 ALTER TABLE animals DROP COLUMN species, ADD COLUMN species_id INT, ADD COLUMN owner_id INT;
